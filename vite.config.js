dd7e6514cc83a08c7e44720a1bc99a148e2c1cfa
Isramss <isra.msadak@gmail.com>
2024-02-02 15:40:35 +0100

    deploy -

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Jean-Neymar/",
+  // base: "/Jean-Neymar/",
 });
